21714aa47ac5698de1cc22009b9e358d28d02302
Diogo Pinto <diogosmp@gmail.com>
2020-02-04 00:04:26 +0000

    Created head.js

---
+++ head.js
@@ -0,0 +1,14 @@
+// ASSERT EQUAL
+const assertEqual = function(actual, expected) {
+  console.log((actual === expected) ? `🟢🟢🟢 Assertion Passed: ${actual} === ${expected}` : `🔴🔴🔴 Assertion Failed: ${actual} !== ${expected}`);
+};
+
+// Create a function head which returns the first item in the array.
+const head = arr => arr[0];
+
+
+// TESTING
+assertEqual(head([5,6,7]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+assertEqual(head([]),undefined)
+assertEqual(head("blahblah"),undefined)
