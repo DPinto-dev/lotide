a23a1ffb8db25ec625f7fc182f8e922af0a0cf75
Diogo Pinto <diogosmp@gmail.com>
2020-02-05 00:31:53 +0000

    Created a function to compare every element of two arrays, as part of the "Lotide" project

---
+++ eqArrays.js
@@ -0,0 +1,25 @@
+// 36
+const assertEqual = function(actual, expected) {
+  console.log((actual === expected) ? `🟢🟢🟢 Assertion Passed: ${actual} === ${expected}` : `🔴🔴🔴 Assertion Failed: ${actual} !== ${expected}`);
+};
+
+// Implement a function eqArrays which takes in two arrays and returns true or false, based on a perfect match.f\
+const eqArrays = (arr1, arr2) => {
+  if (arr1.length !== arr2.length) return false;
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) return false;
+  }
+  return true;
+};
+
+
+
+// Tests
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true); // => true
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]),false);
+
+// console.log(eqArrays([1, 2, 3], [1, 2, 3])) // => true
+// console.log(eqArrays([1, 2, 3], [3, 2, 1]) )// => false
+// console.log(eqArrays(["1", "2", "3"], ["1", "2", "3"])) // => true
+// console.log(eqArrays(["1", "2", "3"], ["1", "2", 3]) )// => false
