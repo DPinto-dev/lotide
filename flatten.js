0b8d56659da8a3f475fa470f49da0f5e69009287
Diogo Pinto <diogosmp@gmail.com>
2020-02-05 02:30:09 +0000

    Created flatten.js that returns a flattened version of an array of arrays

---
+++ flatten.js
@@ -0,0 +1,31 @@
+const eqArrays = (arr1, arr2) => {
+  if (arr1.length !== arr2.length) return false;
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) return false;
+  }
+  return true;
+};
+
+const assertArraysEqual = (arr1, arr2) => {
+  if (eqArrays(arr1, arr2)) {
+    console.log("🟢🟢🟢 Assertion Passed: ", arr1, "===", arr2);
+  } else {
+    console.log("🔴🔴🔴 Assertion Failed: ", arr1, "!==", arr2);
+  }
+};
+
+//Create a function flatten which will take in an array of arrays and return a "flattened" version of the array.
+const flatten = arr => {
+  let output = [];
+  for (let outer of arr) {
+    if (Array.isArray(outer)) {
+      output.push(...outer);
+    } else output.push(outer);
+  }
+  return output;
+};
+
+// TESTS
+assertArraysEqual(flatten([1, 2, [3, 4], 5, [6]]), [1, 2, 3, 4, 5, 6]); // passes
+assertArraysEqual(flatten([1, 2, 3, ["a", "b", "c"], 4, 5, 6, 7, ["h", "i"]]), [ 1, 2, 3, 'a', 'b', 'c', 4, 5, 6, 7, 'h', 'i']); // passes
+
