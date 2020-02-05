a037c108cbf3086b5c860a5b896249666a0ca379
Diogo Pinto <diogosmp@gmail.com>
2020-02-05 21:04:21 +0000

    Created countLetters.js

---
+++ countLetters.js
@@ -0,0 +1,19 @@
+const assertEqual = function(actual, expected) {
+  console.log((actual === expected) ? `🟢🟢🟢 Assertion Passed: ${actual} === ${expected}` : `🔴🔴🔴 Assertion Failed: ${actual} !== ${expected}`);
+};
+
+// Create a function called countLetters, that takes in a sentence (as a string) and then return a count of each of the letters in that sentence.
+
+const countLetters = str => {
+  let returnObj = {};
+  for (const letter of str) {
+    if (letter !== ' ') returnObj[letter] = (returnObj[letter] || 0) + 1;
+  }
+  return returnObj;
+};
+
+
+console.log(countLetters("Di    ogo"));
+const result = countLetters("lighthouse in the house");
+assertEqual(result["h"], 4);
+assertEqual(result["o"], 2);
