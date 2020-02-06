d45d45391dad237d1604059d5561420fe8597793
Diogo Pinto <diogosmp@gmail.com>
2020-02-06 22:45:53 +0000

    Modified takeUntil.js to include assertArraysEqual and proper test examples

--- takeUntil.js
+++ takeUntil.js
@@ -1,3 +1,5 @@
+const assertArraysEqual = require('./assertArraysEqual');
+
 // Implement takeUntil which will keep collecting items from a provided array until the callback provided returns a truthy value.
 const takeUntil = (arr, cb) => {
   const results = [];
@@ -10,10 +12,13 @@ const takeUntil = (arr, cb) => {
 
 const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
 const results1 = takeUntil(data1, x => x < 0);
-console.log(results1);
+// console.log(results1);
 
-console.log('---');
+// console.log('---');
 
 const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
 const results2 = takeUntil(data2, x => x === ',');
-console.log(results2);
+// console.log(results2);
+
+assertArraysEqual(results1, [ 1, 2, 5, 7, 2 ]);
+assertArraysEqual(results2, [ 'I\'ve', 'been', 'to', 'Hollywood' ]);
