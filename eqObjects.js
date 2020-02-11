6e73b8a166c12d79263db5a22d4f702b6003c3df
Diogo Pinto <diogosmp@gmail.com>
2020-02-11 03:00:48 +0000

    Wrote eqObjects recursively and got rid of eqArrays calls

--- eqObjects.js
+++ eqObjects.js
@@ -1,53 +1,22 @@
-const assertEqual = function(actual, expected) {
-  console.log((actual === expected) ? `🟢🟢🟢 Assertion Passed: ${actual} === ${expected}` : `🔴🔴🔴 Assertion Failed: ${actual} !== ${expected}`);
-};
-
-const eqArrays = (arr1, arr2) => {
-  if (arr1.length !== arr2.length) return false;
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) return false;
-  }
-  return true;
-};
-
-// Implement the definition for function eqObjects which will take in two objects and returns true or false, based on a perfect match.
-
-// Returns true if both objects have identical keys with identical values.
-// Otherwise you get back a big fat false!
+// eqObjects takes in two objects and returns true if both objects have identical keys with identical values.
 const eqObjects = function(object1, object2) {
   if (Object.keys(object1).length !== Object.keys(object2).length) {
     return false;
-  }
-
-  // Compare the values of both objects per each key 
-  for (key of Object.keys(object1)) {
-    if (Array.isArray(object1[key])) {
-      return eqArrays(object1[key],object2[key]);
-    }
-    else if (object1[key] !== object2[key]) {
-      return false
+  } else {
+    for (const key in object1) {
+      // check if it's an object and recursively call the function
+      if (typeof object1[key] === 'object' && object1[key] !== null) {
+        if (!eqObjects(object1[key], object2[key])) {
+          return false
+        }
+      }
+      // Base Case: single level object
+      else if (object1[key] !== object2[key]) {
+        return false
+      }
     }
   }
   return true;
 };
 
-
-// TESTS
-// const ab = { a: "1", b: "2" };
-// const ba = { b: "2", a: "1" };
-// assertEqual(eqObjects(ab, ba), true); // => true
-// const abc = { a: "1", b: "2", c: "3" };
-// assertEqual(eqObjects(ab, abc), false); // => false
-
-
-// const cd = { c: "1", d: ["2", 3] };
-// const dc = { d: ["2", 3], c: "1" };
-// assertEqual(eqObjects(cd, dc), true) // => true
-
-// const cd2 = { c: "1", d: ["2", 3, 4] };
-// assertEqual(eqObjects(cd, cd2), false)// => false
-
-
-assertEqual(eqObjects({ a: { z: 1 }, b: 2 }, { a: { z: 1 }, b: 2 })); // => true
-assertEqual(eqObjects({ a: { y: 0, z: 1 }, b: 2 }, { a: { z: 1 }, b: 2 })) // => false
-assertEqual(eqObjects({ a: { y: 0, z: 1 }, b: 2 }, { a: 1, b: 2 })) // => false
+module.exports = eqObjects;
